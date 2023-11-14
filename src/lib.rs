30ed427a9c72583b07c4d2922a771235f6fa661e
Dan Dixey <dan@sigmafinancial.ai>
2023-11-14 00:04:34 +0000

    doc: Init documentation site

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! LP Parser
+//!
+//! This is a parser for the [LP](https://en.wikipedia.org/wiki/Linear_programming) file format leveraging the [pest](https://pest.rs) crate for parsing.
+//!
+//! The PEST file has been derived from the following resources:
+//! - [IBM v22.1.1 Specification](https://www.ibm.com/docs/en/icos/22.1.1?topic=cplex-lp-file-format-algebraic-representation)
+//! - [fico](https://www.fico.com/fico-xpress-optimization/docs/dms2020-03/solver/optimizer/HTML/chapter10_sec_section102.html)
+//! - [Gurobi](https://www.gurobi.com/documentation/current/refman/lp_format.html)
+//!
+//! It supports the following LP file features:
+//! - Problem Name
+//! - Problem Sense
+//! - Objectives
+//!   - Single-Objective Case
+//!   - Multi-Objective Case
+//! - Constraints
+//! - Bounds
+//! - Variable Types: Integer, Generals, Lower Bounded, Upper Bounded, Free & Upper and Lower Bounded
+//! - Semi-continuous
+//! - Special Order Sets (SOS)
+//!
+
 #![deny(
     rust_2018_idioms,
     unused_must_use,
