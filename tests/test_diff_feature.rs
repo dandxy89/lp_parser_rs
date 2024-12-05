fb004cfeb6ca007bc3217fdca2fc0ffb8bb97f35
Dan Dixey <dan@sigmafinancial.ai>
2024-12-05 22:44:20 +0000

    test: Ensure the diff tests are actually run

--- tests/test_diff_feature.rs
+++ tests/test_diff_feature.rs
@@ -3,6 +3,7 @@
 
 use std::path::PathBuf;
 
+use diff::Diff as _;
 use lp_parser_rs::{
     model::{coefficient::Coefficient, constraint::Constraint, lp_error::LPParserError, lp_problem::LPProblem, sense::Cmp},
     parse::{parse_file, parse_lp_file},
@@ -10,8 +11,6 @@ use lp_parser_rs::{
 
 #[test]
 fn test_coefficient() {
-    use diff::Diff;
-
     let a = Coefficient { var_name: "a".to_string(), coefficient: 1.0 };
     let b = Coefficient { var_name: "b".to_string(), coefficient: 1.0 };
     insta::assert_yaml_snapshot!(a.diff(&a));
@@ -20,8 +19,6 @@ fn test_coefficient() {
 
 #[test]
 fn test_constraint() {
-    use diff::Diff;
-
     let a = Constraint::Standard {
         name: "a".to_string(),
         coefficients: vec![Coefficient { var_name: "ca".to_string(), coefficient: 1.0 }],
@@ -40,8 +37,6 @@ fn test_constraint() {
 
 #[test]
 fn test_file_comparison() {
-    use diff::Diff;
-
     let a = read_file_from_resources("test.lp").unwrap();
     let a_copy = read_file_from_resources("test_copy.lp").unwrap();
     let b = read_file_from_resources("test2.lp").unwrap();
@@ -49,7 +44,7 @@ fn test_file_comparison() {
     insta::assert_yaml_snapshot!(a.diff(&a), {
         ".variables.altered" => insta::sorted_redaction(),
         ".variables.removed" => insta::sorted_redaction(),
-        ".objectives.Altered" => insta::sorted_redaction(),
+        ".objectives.Removed" => insta::sorted_redaction(),
         ".objectives.Altered" => insta::sorted_redaction(),
         ".constraints.altered" => insta::sorted_redaction(),
         ".constraints.removed" => insta::sorted_redaction(),
@@ -58,7 +53,7 @@ fn test_file_comparison() {
     insta::assert_yaml_snapshot!(a.diff(&a_copy), {
         ".variables.altered" => insta::sorted_redaction(),
         ".variables.removed" => insta::sorted_redaction(),
-        ".objectives.Altered" => insta::sorted_redaction(),
+        ".objectives.Removed" => insta::sorted_redaction(),
         ".objectives.Altered" => insta::sorted_redaction(),
         ".constraints.altered" => insta::sorted_redaction(),
         ".constraints.removed" => insta::sorted_redaction(),
@@ -67,7 +62,7 @@ fn test_file_comparison() {
     insta::assert_yaml_snapshot!(a.diff(&b), {
         ".variables.altered" => insta::sorted_redaction(),
         ".variables.removed" => insta::sorted_redaction(),
-        ".objectives.Altered" => insta::sorted_redaction(),
+        ".objectives.Removed" => insta::sorted_redaction(),
         ".objectives.Altered" => insta::sorted_redaction(),
         ".constraints.altered" => insta::sorted_redaction(),
         ".constraints.removed" => insta::sorted_redaction(),
